7b580a2722e9dd6109d3ae14cc35a6fb9a54a86b
nickj <consultor.nicholas@leomadeiras.com.br>
2022-10-24 11:19:18 -0300

    setup estlint, prettier, and typescript config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+
+import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +10,6 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
